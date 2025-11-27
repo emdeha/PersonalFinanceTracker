a05e5b64d8392a3010b89a3ad5276cb1113f8340
Tsvetan Tsvetanov <tsvetan@camplight.net>
2025-11-27 17:59:14 +0200

    Simplifies App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
 import './App.css'
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router"
 
 function App() {
-  return <RouterProvider router={router} />
+  return <Home />
 }
 
 function Home() {
   return (
     <>
       <div>Home</div>
-      <Outlet />
     </>
   )
 }
-
-function PageOne() {
-  return <div>Page One</div>
-}
-
-function PageTwo() {
-  return <div>Page Two</div>
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: Home,
-    children: [
-      {
-        path: 'pageone',
-        Component: PageOne,
-      },
-      {
-        path: 'pagetwo',
-        Component: PageTwo,
-      },
-    ],
-  },
-])
 
 export default App
